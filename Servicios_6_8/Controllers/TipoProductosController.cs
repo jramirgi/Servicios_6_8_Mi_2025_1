9207af4ec70929ffd33763fff64eef60d525475c
JOSE IGNACIO RAMIREZ GIRALDO <joseramirez8046@correo.itm.edu.co>
2025-03-05 06:19:07 -0500

    Se modifica la clase TipoProducto y se agregar el controlador para los tipos de productos

---
+++ Servicios_6_8/Controllers/TipoProductosController.cs
@@ -0,0 +1,45 @@
+﻿using Servicios_6_8.Clases;
+using Servicios_6_8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Servicios_6_8.Controllers
+{
+    [RoutePrefix("api/TipoProductos")]
+    public class TipoProductosController : ApiController
+    {
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<TIpoPRoducto> ConsultarTodos()
+        {
+            clsTipoProducto TipoProducto = new clsTipoProducto();
+            return TipoProducto.ConsultarTodos();
+        }
+        [HttpGet]
+        [Route("Consultar")]
+        public TIpoPRoducto Consultar(int Codigo)
+        {
+            clsTipoProducto TipoProducto = new clsTipoProducto();
+            return TipoProducto.Consultar(Codigo);
+        }
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] TIpoPRoducto TipoProducto)
+        {
+            clsTipoProducto tipoProducto = new clsTipoProducto();
+            tipoProducto.TipoProducto = TipoProducto;
+            return tipoProducto.Insertar();
+        }
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] TIpoPRoducto TipoProducto)
+        {
+            clsTipoProducto tipoProducto = new clsTipoProducto();
+            tipoProducto.TipoProducto = TipoProducto;
+            return tipoProducto.Actualizar();
+        }
+}
