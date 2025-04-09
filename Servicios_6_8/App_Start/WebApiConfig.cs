cdfaa0d250282b002b2759f7bb5e5537616d60fe
joseramirez <joseramirez8046@correo.itm.edu.co>
2025-04-09 06:31:40 -0500

    Se agrega el proceso de autenticación

--- Servicios_6_8/App_Start/WebApiConfig.cs
+++ Servicios_6_8/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Servicios_6_8.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,6 +11,7 @@ namespace Servicios_6_8
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.MessageHandlers.Add(new TokenValidationHandler());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
